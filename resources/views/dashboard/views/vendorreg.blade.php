4f7c54a59963ca7cac2ea074459fe19f8213ae0f
Salhaan Sallu <salhaansoft@gmail.com>
2023-08-24 13:08:56 +0530

    Minor bugs fixed

--- resources/views/dashboard/views/vendorreg.blade.php
+++ resources/views/dashboard/views/vendorreg.blade.php
@@ -20,7 +20,7 @@
 
 <div class="vendor_form">
     <div class="header">
-        <h1>Vendor registor</h1>
+        <h1>Manufacturer register</h1>
     </div>
     <form action="" onsubmit="return false;" method="post" id="vendorRegister">
         @csrf
@@ -33,7 +33,7 @@
                     <input type="text" name="name" id="name" placeholder="Enter company name" required>
                 </div>
             </div>
-    
+
             <div class="input">
                 <div class="label">Company email</div>
                 <div class="txt_field">
